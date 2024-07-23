85c6da95ab148521e7b6a4da2a3567a42c9e331a
SlowFlo <fbautry@gmail.com>
2024-07-24 00:47:44 +0200

    refactor: span for the name and emoji

--- 00-challenge/01-profile-card-v1/src/index.js
+++ 00-challenge/01-profile-card-v1/src/index.js
@@ -1,44 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
-    return <div className="card">
-        <Avatar/>
-        <div className="data">
-            <Intro/>
-            <SkillList/>
-        </div>
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
     </div>
+  );
 }
 
 function Avatar() {
-    return <img className="avatar" src="avatar.jpg" alt="Me in Japan"/>
+  return <img className="avatar" src="avatar.jpg" alt="Me in Japan" />;
 }
 
 function Intro() {
-    return <div>
-        <h1>Florian Bautry</h1>
-        <p>Futur full-stack developper with Django and React. Building apps with TDD magic from Japan.</p>
+  return (
+    <div>
+      <h1>Florian Bautry</h1>
+      <p>
+        Futur full-stack developper with Django and React. Building apps with
+        TDD magic from Japan.
+      </p>
     </div>
+  );
 }
 
 function SkillList() {
-    return <div className="skill-list">
-        <Skill name="JavaScript" emoji="👑" backgroundColor="yellow"/>
-        <Skill name="Python" emoji="🐍" backgroundColor="green"/>
-        <Skill name="TDD" emoji="💉" backgroundColor="red"/>
-        <Skill name="Japanese" emoji="🗾" backgroundColor="antiquewhite"/>
+  return (
+    <div className="skill-list">
+      <Skill name="JavaScript" emoji="👑" color="yellow" />
+      <Skill name="Python" emoji="🐍" color="green" />
+      <Skill name="TDD" emoji="💉" color="red" />
+      <Skill name="Japanese" emoji="🗾" color="antiquewhite" />
     </div>
+  );
 }
 
 function Skill(props) {
-    return <span className="skill" style={{backgroundColor: props.backgroundColor}}>{props.name}{props.emoji}</span>
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.name}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
