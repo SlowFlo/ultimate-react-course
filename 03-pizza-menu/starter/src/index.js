ae76552a526d738ea22c341bbcfdd9a054582102
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-07-24 10:27:11 +0200

    feat: setting classes and text conditionaly

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -80,13 +80,13 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
-      {/*{numPizzas > 0 && (*/}
-      {/*  <ul className="pizzas">*/}
-      {/*    {pizzas.map((pizza) => (*/}
-      {/*      <Pizza pizzaObj={pizza} key={pizza.name} />*/}
-      {/*    ))}*/}
-      {/*  </ul>*/}
-      {/*)}*/}
+      {/*{numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}*/}
 
       {numPizzas > 0 ? (
         // <React.Fragment key="jejej"></React.Fragment>
@@ -106,19 +106,19 @@ function Menu() {
         <p>We're still working on our menu. Please come back later :)</p>
       )}
 
-      {/*<Pizza*/}
-      {/*  name="Pizza Spinaci"*/}
-      {/*  ingredients="Tomato, mozarella, spinach, and ricotta cheese"*/}
-      {/*  photoName="pizzas/spinaci.jpg"*/}
-      {/*  price={10}*/}
-      {/*/>*/}
+      {/*<Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />*/}
 
-      {/*<Pizza*/}
-      {/*  name="Pizza Funghi"*/}
-      {/*  ingredients="Tomato, mushrooms, and onion"*/}
-      {/*  price={12}*/}
-      {/*  photoName="pizzas/funghi.jpg"*/}
-      {/*/>*/}
+      {/*<Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms, and onion"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />*/}
     </main>
   );
 }
@@ -126,15 +126,22 @@ function Menu() {
 function Pizza({ pizzaObj }) {
   console.log(pizzaObj);
 
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+
+        {/*{pizzaObj.soldOut ? (
+          <span>"SOLD OUT"</span>
+        ) : (
+          <span>pizzaObj.price</span>
+        )}*/}
+
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
