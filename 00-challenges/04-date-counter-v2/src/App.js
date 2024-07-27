add3023008d3225648e40b6e34e713c9cad6230d
SlowFlo <fbautry@gmail.com>
2024-07-27 16:41:34 +0200

    feat: my date counter V2 solution

---
+++ 00-challenges/04-date-counter-v2/src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date(Date.now());
+  date.setDate(date.getDate() + count);
+
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
+
+  return (
+    <>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        ></input>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>{count === 0 && "Today is "}</span>
+        <span>{count < 0 && Math.abs(count) + " days ago was "}</span>
+        <span>{count > 0 && count + " days from now is "}</span>
+        <span>{date.toDateString()}</span>
+      </p>
+
+      {step !== 1 || count !== 0 ? (
+        <button onClick={handleReset}>Reset</button>
+      ) : null}
+    </>
+  );
+}
