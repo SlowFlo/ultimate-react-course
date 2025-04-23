1ba40aa44112fcb1ac7b4e6ed472a9a6f5ca6463
SlowFlo <fbautry@gmail.com>
2025-04-23 22:17:05 +0200

    feat: Restarting a Quiz

--- 10-react-quiz/react-quiz/src/components/App.jsx
+++ 10-react-quiz/react-quiz/src/components/App.jsx
@@ -58,6 +58,13 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+        highscore: state.highscore,
+      };
 
     default:
       throw new Error("Action unkonwn");
@@ -120,6 +127,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblepoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
