5b56c3c12a9848e437dcd3b41dff4759b69481ad
SlowFlo <fbautry@gmail.com>
2025-04-28 00:13:47 +0200

    feat: Implementing Main Pages and Routes

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product.jsx";
+import Pricing from "./pages/Pricing.jsx";
+import Homepage from "./pages/Homepage.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
 
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
