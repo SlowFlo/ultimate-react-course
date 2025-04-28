daa911e9747ca48ea9ecc6b4a409fc6076fdd897
SlowFlo <fbautry@gmail.com>
2025-04-29 00:36:35 +0200

    feat: Using CSS Modules

---
+++ 11-worldwise/worldwise/src/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AppNav from "../components/AppNav.jsx";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>APP</p>
+    </div>
+  );
+}
+
+export default AppLayout;
