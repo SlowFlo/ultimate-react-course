c669d740f167355c91a7913cf900fb7881e1218b
SlowFlo <fbautry@gmail.com>
2025-05-04 20:07:26 +0200

    feat: Nested Routes and Index Route

--- 11-worldwise/worldwise/src/components/Sidebar.jsx
+++ 11-worldwise/worldwise/src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import styles from "./Sidebar.module.css";
 import Logo from "./Logo.jsx";
 import AppNav from "./AppNav.jsx";
 import Footer from "./Footer.jsx";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
@@ -10,7 +11,7 @@ function Sidebar() {
       <Logo />
       <AppNav />
 
-      <p>List of cities</p>
+      <Outlet />
 
       <Footer />
     </div>
