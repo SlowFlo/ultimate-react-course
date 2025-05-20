02b1e7cfc476f4e8a9d99db6a6dc6ca932d93b0a
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-05-20 13:49:41 +0200

    feat: Adding Fake Authentication: Protecting a Route

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -15,6 +15,7 @@ import City from "./components/City.jsx";
 import Form from "./components/Form.jsx";
 import { CitiesProvider } from "./contexts/CitiesContext.jsx";
 import { AuthProvider } from "./contexts/FakeAuthContext.jsx";
+import ProtectedRoute from "./pages/ProtectedRoute.jsx";
 
 polyfillCountryFlagEmojis();
 
@@ -28,7 +29,14 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
