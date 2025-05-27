02316ad7bc965ea267b018791c52fb989ed12278
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-05-27 17:49:58 +0200

    feat: Optimizing Bundle Size With Code Splitting

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import Product from "./pages/Product.jsx";
-import Pricing from "./pages/Pricing.jsx";
-import Homepage from "./pages/Homepage.jsx";
-import PageNotFound from "./pages/PageNotFound.jsx";
-import AppLayout from "./pages/AppLayout.jsx";
-import Login from "./pages/Login.jsx";
-import CityList from "./components/CityList.jsx";
-import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
-
-import CountryList from "./components/CountryList.jsx";
-import City from "./components/City.jsx";
-import Form from "./components/Form.jsx";
 import { CitiesProvider } from "./contexts/CitiesContext.jsx";
 import { AuthProvider } from "./contexts/FakeAuthContext.jsx";
 import ProtectedRoute from "./pages/ProtectedRoute.jsx";
+
+import CityList from "./components/CityList.jsx";
+import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
+import CountryList from "./components/CountryList.jsx";
+import City from "./components/City.jsx";
+import Form from "./components/Form.jsx";
+import SpinnerFullPage from "./components/SpinnerFullPage.jsx";
+
+// import Product from "./pages/Product.jsx";
+// import Pricing from "./pages/Pricing.jsx";
+// import Homepage from "./pages/Homepage.jsx";
+// import PageNotFound from "./pages/PageNotFound.jsx";
+// import AppLayout from "./pages/AppLayout.jsx";
+// import Login from "./pages/Login.jsx";
+
+const Homepage = lazy(() => import("./pages/Homepage.jsx"));
+const Product = lazy(() => import("./pages/Product.jsx"));
+const Pricing = lazy(() => import("./pages/Pricing.jsx"));
+const Login = lazy(() => import("./pages/Login.jsx"));
+const AppLayout = lazy(() => import("./pages/AppLayout.jsx"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound.jsx"));
+
+// dist/assets/index-93dbd7c8.css   30.32 kB │ gzip:   5.07 kB
+// dist/assets/index-0b92051f.js   510.48 kB │ gzip: 149.17 kB
 
 polyfillCountryFlagEmojis();
 
@@ -24,27 +36,29 @@ function App() {
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="product" element={<Product />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="login" element={<Login />} />
-            <Route
-              path="app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate replace to="cities" />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route index element={<Navigate replace to="cities" />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
