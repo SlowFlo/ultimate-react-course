388652965a2675f4143f44f5e93b1e33abc8ccb4
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-06-03 18:40:55 +0200

    feat: Working With Action Creators

--- 15-redux-intro/redux-intro/src/store.js
+++ 15-redux-intro/redux-intro/src/store.js
@@ -35,15 +35,44 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-store.dispatch({ type: "account/withdraw", payload: 200 });
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
+// console.log(store.getState());
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+// In old code bases you can see a separate files with all the type strings put inside constants
+// const ACCOUNT_DEPOSIT = "account/deposit"
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
 console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
+store.dispatch(requestLoan(1000, "Buy a car"));
 console.log(store.getState());
-
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(payLoan());
 console.log(store.getState());
