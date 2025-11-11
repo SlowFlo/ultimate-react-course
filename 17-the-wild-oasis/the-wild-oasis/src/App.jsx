b5c85f21e29920a9ed35a058833d927539a99e60
SlowFlo <fbautry@gmail.com>
2025-11-11 15:46:35 +0100

    feat: Fetching Cabin Data

--- 17-the-wild-oasis/the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/App.jsx
@@ -13,7 +13,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
-  defaultOptions: { queries: { staleTime: 60 * 1000 } },
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime: 0,
+    },
+  },
 });
 
 function App() {
