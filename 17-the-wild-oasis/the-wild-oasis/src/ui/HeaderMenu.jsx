5a7f67ad719b20d81e1d39c9b4d943fcba59aa27
SlowFlo <fbautry@gmail.com>
2026-05-16 21:10:02 +0200

    feat: Implementing Dark Mode With CSS Variables

--- 17-the-wild-oasis/the-wild-oasis/src/ui/HeaderMenu.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/ui/HeaderMenu.jsx
@@ -3,6 +3,7 @@ import ButtonIcon from "./ButtonIcon.jsx";
 import { HiOutlineUser } from "react-icons/hi2";
 import Logout from "../features/authentication/Logout.jsx";
 import { useNavigate } from "react-router-dom";
+import DarkModeToggle from "./DarkModeToggle.jsx";
 
 const StyledHeaderMenu = styled.ul`
   display: flex;
@@ -18,6 +19,9 @@ function HeaderMenu() {
         <ButtonIcon onClick={() => navigate("/account")}>
           <HiOutlineUser />
         </ButtonIcon>{" "}
+      </li>
+      <li>
+        <DarkModeToggle />
       </li>
       <li>
         <Logout />
