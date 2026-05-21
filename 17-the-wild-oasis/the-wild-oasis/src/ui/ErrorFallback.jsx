c8d89f5041e55dff8f901019d43e1a6a640a6962
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2026-05-21 18:45:33 +0200

    feat: Error Boundaries

--- 17-the-wild-oasis/the-wild-oasis/src/ui/ErrorFallback.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/ui/ErrorFallback.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Heading from "./Heading.jsx";
+import GlobalStyles from "../styles/GlobalStyles.js";
+import Button from "./Button.jsx";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +32,23 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <>
+      {/* As we are outside the App we need to import back the global styles*/}
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong 🧐</Heading>
+          <p>{error.message}</p>
+          <Button $size="large" onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+}
+
+export default ErrorFallback;
