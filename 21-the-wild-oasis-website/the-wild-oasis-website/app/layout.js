6ecf8a76713a26906e5087520c01330b701119b5
slowflo <fbautry@gmail.com>
2026-05-29 20:56:46 +0200

    feat: Improving the Navigation and Root Layout

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/layout.js
@@ -1,7 +1,6 @@
-import Navigation from "@/app/_components/Navigation";
-import Logo from "@/app/_components/Logo";
 import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
+import Header from "@/app/_components/Header";
 
 const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 
@@ -19,14 +18,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by TheWild Oasis</footer>
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
