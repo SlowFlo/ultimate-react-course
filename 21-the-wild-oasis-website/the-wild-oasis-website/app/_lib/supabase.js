e96fb86dac4d8c3c40dd841d0b3f7fab7f15cf0f
SlowFlo <fbautry@gmail.com>
2026-05-31 23:45:37 +0200

    feat: Fetching and Displaying Cabin List

--- 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/supabase.js
+++ 21-the-wild-oasis-website/the-wild-oasis-website/app/_lib/supabase.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
